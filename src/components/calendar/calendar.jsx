382ec208daf7155877700649595a95daf4ad736a
Christian Go/CAN <christian.go@ca.cushwake.com>
2024-07-08 23:00:11 -0600

    Created getCalendar function

--- src/components/calendar/calendar.jsx
+++ src/components/calendar/calendar.jsx
@@ -1,13 +1,22 @@
+import { useState, useEffect } from 'react';
 import CalendarHeader from "./calendarHeader";
 import DaysOfTheWeek from "./daysOfTheWeek";
 import Day from "./day";
 import { getArrayOfCalendarDays, getMonthNameFromNum } from "../../misc/miscFunctions";
+import { getCalendar } from '../../misc/apiCalls';
 
 export default function Calendar () {
     const curDate = new Date();
     const monthName = getMonthNameFromNum(curDate.getMonth());
     const dayNums = getArrayOfCalendarDays(curDate);
+    const [ data, setData ] = useState([])
 
+    useEffect(() => {
+        getCalendar(7, 2024)
+        .then(data => console.log(data))
+        .then(data => setData(data))
+    }, [])
+    
     return (
         <main className="w-4/5 mx-auto mb-10 border border-solid border-gray-200 rounded-xl">
             <CalendarHeader monthName={ monthName } />
--- src/misc/apiCalls.js
+++ src/misc/apiCalls.js
@@ -1,3 +1,9 @@
 import axios from 'axios';
 
-const url = 'https://'
+const url = 'http://127.0.0.1:8000/api/'
+
+export async function getCalendar (month, year) {
+    const newUrl = url + 'calendar/?month=' + month + '&year=' + year
+    const result = await axios.get(newUrl)
+    return result.data;
+}
