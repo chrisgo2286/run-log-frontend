1c7dda473bd3d91cbc3acb5db39bd74f300e8873
Christian Go <chrisgo2286@yahoo.com>
2024-07-10 02:05:25 -0400

    Worked on calendar

--- src/components/calendar/calendar.jsx
+++ src/components/calendar/calendar.jsx
@@ -14,25 +14,16 @@ export default function Calendar () {
     useEffect(() => {
         getCalendar(7, 2024).then(data => setData(data))
     }, [])
-
-    function getData (dayNum) {
-        const runData = data.filter((run) => {
-            const newDate = new Date(run)
-            console.log(newDate.getDate())
-            return newDate.getDate() === dayNum;
-        })
-    }
     
     return (
         <main className="w-4/5 mx-auto mb-10 border border-solid border-gray-200 rounded-xl">
             <CalendarHeader monthName={ monthName } />
             <DaysOfTheWeek />
             <div className="grid grid-cols-7 border border-solid border-gray-200">
-                { dayNums.map((dayNum, ndx) => (
+                { data.map((dayData, ndx) => (
                     <Day 
                         key={ dayNum + ndx } 
-                        dayNum={ dayNum } 
-                        data={ getData(dayNum) } /> 
+                        dayData={ dayData } /> 
                 ))}
             </div>
             
